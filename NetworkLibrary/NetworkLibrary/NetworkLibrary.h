3f5f0860e743790e02cef43d09313ab58d4659e0
saichandpratapagiri <31931608+saichandpratapagiri@users.noreply.github.com>
2021-03-11 21:54:15 +0530

    initial commit

---
+++ NetworkLibrary/NetworkLibrary/NetworkLibrary.h
@@ -0,0 +1,18 @@
+//
+//  NetworkLibrary.h
+//  NetworkLibrary
+//
+//  Created by Saichand Pratapagiri on 11/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkLibrary.
+FOUNDATION_EXPORT double NetworkLibraryVersionNumber;
+
+//! Project version string for NetworkLibrary.
+FOUNDATION_EXPORT const unsigned char NetworkLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkLibrary/PublicHeader.h>
+
+
---
+++ NetworkLibrary/NetworkLibrary/Service/Servicemanager.swift
@@ -0,0 +1,42 @@
+//
+//  Servicemanager.swift
+//  NetworkLibrary
+//
+//  Created by Saichand Pratapagiri on 11/03/21.
+//
+
+import Foundation
+
+public enum RequestError: String, Error {
+    case parametersMissing = "paraameters are Nil."
+    case encodingFailed = "parameter encoding failed."
+    case missingUrl = "URL is nil."
+}
+
+protocol EndPointType {
+    var baseUrl: URL { get }
+    var path: String { get }
+    var httpMethod: HttpMethod { get }
+    var task: HttpTask { get }
+    var headers: HttpHeaders { get }
+}
+
+public enum HttpMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case patch = "PATCH"
+    case delete = "DELETE"
+}
+
+public enum HttpTask {
+    case request
+    
+    case requestParams(bodyParams: Parameters?, urlParams: Parameters?)
+    
+    case requestParamsWithHeaders(bodyParams: Parameters?, urlParams: Parameters?, additionalHeaders: HttpHeaders?)
+}
+
+public typealias HttpHeaders = [String : String]
+
+
